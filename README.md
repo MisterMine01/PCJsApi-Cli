bfa27771c107a15fc5df6e6bd5e9a69bbe7497c3
abouget <abouget68@gmail.com>
2023-01-14 20:10:08 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PCJsApi-Cli
+
+
+[json](https://github.com/nlohmann/jsonhttps://github.com/nlohmann/json)
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "nlohmann/json.hpp"
+
+int main(int arc, char** argv) {
+    std::cout << "Hello, world!\n";
+}
